3abed99ede8bb4073f93f9794f83d1fcdc9febcb
Harsh Sharma <sharsh2222@gmail.com>
2021-04-14 18:46:46 +0530

    Initial Code for Authentication

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+pwd = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "loginpage"
+login_manager.login_message = "You are mot authorised to access this page. Please login first."
+login_manager.login_message_category = "danger" 
+
+from app import routes
